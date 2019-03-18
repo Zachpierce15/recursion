e1b4aa1cbdfe969e4a08cdacb374730ae669471a
Zachary <zachary.15pierce@gmail.com>
2019-03-17 18:14:51 -0600

    Finished the the JSON.Stringify

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,43 +5,6 @@
 
 // But instead we're going to implement it from scratch:
 
-
-// var getElementsByClassName = function(className) {
-//  var answer = []
-//  // create an answer array
-//   var listOfNodes = document.body.childNodes;
-//   // console.log(listOfNodes)
-
-
-//   //create a list of all the nodes inside the document
-//   for (var i = 0; i < listOfNodes.length; i++) {
-//     if (listOfNodes[i].classList === undefined) {
-//     	continue
-//     } else if (listOfNodes[i].classList.contains(className)){
-//     	answer.push(listOfNodes[i])
-//     }
-//     if(listOfNodes[i].hasChildNodes()) {
-//        recursive(listOfNodes[i])
-//     }
-//   }
-//   console.log(answer)
-// };
-
-
-
-
-// var recursive = function(node) {
-
-// 	 var nodeChildren = node.childNodes;
-// 	 for (var i = 0; i < nodeChildren.length; i++) {
-// 		if (nodeChildren[i].classList === undefined){
-// 			continue
-// 		} else if (nodeChildren[i].classList.contains(className)){
-// 	  answer.push(nodeChildren[i])
-// 		}
-// 	 }
-// }
-
 var getElementsByClassName = function(className) {
 	return recursiveGetElementsByClassName(document.body, className);
 }
@@ -73,37 +36,6 @@ var nodeHasClassName = function(node, className) {
 	}
 }
 
-// var chasesGetElementsByClassName = function(className) {
-// 	let result = [];
-// 	let nodesToCheck = [];
-
-// 	nodesToCheck.push(document.body);
-
-// 	while (nodesToCheck.length) {
-// 		const nodeToCheck = nodesToCheck.shift();
-// 		if (nodeHasClassNameIterative(nodeToCheck, className)) {
-// 			result.push(nodeToCheck);
-// 		}
-
-// 		const nodeImmediateChildren = nodeToCheck.childNodes;
-// 		for (let i = 0; i < nodeImmediateChildren.length; i++) {
-// 			const child = nodeImmediateChildren[i];
-// 			nodesToCheck.push(child);
-// 		}
-// 	}
-
-// 	return result;
-// }
-
-// var nodeHasClassNameIterative = function(node, className) {
-// 	const classes = node.classList;
-
-// 	if (node.classList === undefined) {
-// 		return false;
-// 	} else {
-// 		return node.classList.contains(className);
-// 	}
-// }
 
 
 
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,87 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "number" || typeof obj === "boolean" || obj === null){
+  	return isNumOrBoolean(obj);
+  } else if(typeof obj === "string") {
+  	return changeStringJSON(obj);
+  } else if (Array.isArray(obj)){
+  	return changeArrayJSON(obj);
+  }else if (typeof obj === "object") {
+  	return objectJSON(obj);
+  }
 };
+
+var isNumOrBoolean = function (obj) {
+	if (typeof obj === "number" || typeof obj === "boolean"){
+		return obj.toString();
+	} else if( obj === null) {
+		return 'null'
+	}
+}
+ var changeStringJSON = function (obj) {
+ 	var split = obj.split('')
+ 	split.unshift('"');
+ 	split.push('"')
+ 	return split.join('')
+ }
+var changeArrayJSON = function(obj) {
+	if (obj.length === 0) {
+		return '[]'
+	} 
+	else {
+		for (var i = 0; i < obj.length; i++) {
+			if (typeof obj[i] === "string") {
+				obj[i] = changeStringJSON(obj[i]);
+				
+			}
+			else if (Array.isArray(obj[i]) === true) {
+				obj[i] = changeArrayJSON(obj[i]);
+			}
+			 else if (typeof obj[i] === "object"){
+               obj[i] = objectJSON(obj[i])
+			}
+			
+		}
+	}
+	return '[' + obj + ']'
+
+}
+var objectJSON = function(obj) {
+	var objString = '';
+	 var key = Object.keys(obj);
+	 var value = Object.values(obj);
+
+	if (Object.keys(obj).length === 0) {
+		return '{}';
+	} 
+     for (var i = 0; i < key.length; i++){
+  			var currKey = key[i]
+        if (typeof value[i] === "function" || typeof value[i] === "undefined"){
+        	return '{}'
+        }
+    	if (typeof value[i] === "boolean" || typeof value[i] === "number" || value[i] === null){
+     		 objString += `"${currKey}":${value[i]},`
+    	} 
+    	else if (typeof value[i] === "object" && Array.isArray(value[i]) !== true && Object.keys(value[i]).length === 0){
+               objString += `"${currKey}":` + objectJSON(value[i])+ ','
+    	}
+    	else if (typeof value[i] === "object" && Array.isArray(value[i]) !== true){
+    		objString += `"${currKey}":` + objectJSON(value[i])+ '}'
+    	}
+    	else if (Array.isArray(value[i])){
+    		for (var j = 0; j < value[i].length; j++){
+    			value[i][j] = '"'+ value[i][j] + '"'
+    		}
+           objString += `"${currKey}":[${value[i]}],`
+    	}
+    	else {
+  			 objString += `"${currKey}":"${value[i]}",`
+	} 
+}
+   objString = objString.split('');
+   objString.pop('')
+   objString = objString.join('')
+	return '{' + objString + '}'
+
+}
